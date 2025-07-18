915fc2d65753af12abbb910424d840e882b1f9c7
nt9 <dn@Trong-pc.>
2025-07-18 13:43:38 -0400

    work

--- PETINA/examples/5_ML_CIFAR_10_MA5.py
+++ PETINA/examples/5_ML_CIFAR_10_MA5.py
@@ -6,6 +6,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 from tqdm import tqdm
@@ -164,13 +165,13 @@ class SimpleCNN(nn.Module):
     #     x = self.fc2(x)
     #     return x
     def __init__(self):
-            super().__init__()
-            self.conv1 = nn.Conv2d(3, 6, 5)
-            self.pool = nn.MaxPool2d(2, 2)
-            self.conv2 = nn.Conv2d(6, 16, 5)
-            self.fc1 = nn.Linear(16 * 5 * 5, 120)
-            self.fc2 = nn.Linear(120, 84)
-            self.fc3 = nn.Linear(84, 10)
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -332,7 +333,7 @@ if __name__ == "__main__":
     print("===========Parameters for DP Training===========")
     print(f"Running experiments with ε={epsilon}, δ={delta}, γ={gamma}, sensitivity={sensitivity}")
     print(f"Total rounds: {rounds}, epochs per round: {epochs_per_round}")
-    print(f"Seed value for reproducibility: {seed}")
+    # print(f"Seed value for reproducibility: {seed}")
     print(f"Batch size: {batch_size}\n")
 
 
